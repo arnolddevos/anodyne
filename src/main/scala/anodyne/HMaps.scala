faee92bf2e33413d4e4550b436c945c26ebfeb13
Arnold deVos <adv@langdale.com.au>
2016-02-18 08:14:17 +1100

    HMaps Term should always be compared by object identity

--- src/main/scala/anodyne/HMaps.scala
+++ src/main/scala/anodyne/HMaps.scala
@@ -1,8 +1,14 @@
 package anodyne
 
-trait HMaps {
+trait HMaps { hmaps =>
 
-  type Term <: { type Value }
+  type Term <: TermSpec
+
+  abstract class TermSpec { 
+    type Value
+    final override def equals(x: Any) = super.equals(x)
+    final override def hashCode(): Int = super.hashCode()
+  }
 
   trait Row {
     def get(t: Term): Option[t.Value]
@@ -10,11 +16,11 @@ trait HMaps {
   }
 
   trait HMap extends Row { self =>
-    protected def underlying: Map[Term, Any]
+    def underlying: Map[Term, Any]
     def get(t: Term): Option[t.Value] = underlying.get(t).asInstanceOf[Option[t.Value]] 
     def apply(t: Term): t.Value = underlying(t).asInstanceOf[t.Value]
-    def add(t: Term)( a: t.Value): HMap = new HMap { 
-      val underlying = self.underlying.updated(t, a) 
+    def add(t: Term)( v: t.Value): HMap = new HMap { 
+      val underlying = self.underlying.updated(t, v) 
     }
     def remove(t: Term): HMap = new HMap { 
       val underlying = self.underlying - t
@@ -23,10 +29,8 @@ trait HMaps {
   }
 
   object HMap {
-    def apply(): HMap = new HMap {
-      protected def underlying = Map.empty
-    }
-
+    def apply() = empty
+    val empty: HMap = new HMap { def underlying = Map.empty }
     def apply( row: Row, ts: Term* ): HMap = 
       ts.foldLeft(HMap())((m, t) => m.add(t)(row(t)))
   }
